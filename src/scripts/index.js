20771463eed6efc3e99282bb214c464e999d8844
Grzegorz Lipiecki <grzegorzlipiecki07@gmail.com>
2023-01-30 15:43:03 +0100

    add of save button functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,22 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const entry = localStorage.getItem('entry');
+let result = 'Wpisz tekst który zapiszesz.';
+
+if (entry) {
+    console.log(`value entry: ${entry}`);
+    result = entry;
+}
+
+const entryInput = document.querySelector('.description-label--JS');
+entryInput.value = result;
+
+
+
+const button = document.querySelector('.button__save--js')
+
+console.log(button)
+
+button.addEventListener('click', () => {
+    localStorage.setItem('entry', entryInput.value);
+});
