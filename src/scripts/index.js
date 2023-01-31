e95efa431a54e12a4e2f0efac0c1a62396c4fedb
Grzegorz Lipiecki <grzegorzlipiecki07@gmail.com>
2023-01-31 18:47:57 +0100

    Editor update and adding functions in js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,37 @@
 import '../styles/index.scss';
 
-const entry = localStorage.getItem('entry');
-let result = 'Wpisz tekst który zapiszesz.';
+// const entry = localStorage.getItem('entry');
+// let result = 'Wpisz tekst który zapiszesz.';
 
-if (entry) {
-    console.log(`value entry: ${entry}`);
-    result = entry;
-}
+// if (entry) {
+//     console.log(`value entry: ${entry}`);
+//     result = entry;
+// }
 
-const entryInput = document.querySelector('.description-label--JS');
-entryInput.value = result;
+// const entryInput = document.querySelector('.description-label--JS');
+// entryInput.value = result;
 
 
 
-const button = document.querySelector('.button__save--js')
+// const button = document.querySelector('.button__save--js')
 
-console.log(button)
+// console.log(button)
 
-button.addEventListener('click', () => {
-    localStorage.setItem('entry', entryInput.value);
-});
+// button.addEventListener('click', () => {
+//     localStorage.setItem('entry', entryInput.value);
+// });
+
+
+const textarea = document.querySelector(".description-label--js")
+const save = document.querySelector(".button__save--js")
+const load = document.querySelector(".button__load--js")
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('entry', textarea.value);
+})
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');
+})
